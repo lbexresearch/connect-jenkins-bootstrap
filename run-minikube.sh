0a26387b23e323adc08e465633ce4bd417f2075d
Igor Khripunov <igor.khripunov@clearpoint.co.nz>
2017-08-31 10:19:03 +1200

    Added SSH keys

--- run-minikube.sh
+++ run-minikube.sh
@@ -6,4 +6,8 @@ then
 	kubectl create ns jenkins
 fi
 kubectl config set-context $(kubectl config current-context) --namespace=jenkins
+if [[ -z $(kubectl get secrets | grep ssh-key-secret) ]]
+then
+	kubectl create secret generic ssh-key-secret --from-file=ssh-privatekey=./id_rsa --from-file=ssh-publickey=./id_rsa.pub
+fi
 kubectl apply -f jenkins.yml
