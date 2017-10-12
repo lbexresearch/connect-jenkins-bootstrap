8d6411f1375a0cedb4e5c62634da6b00b635da64
Igor Khripunov <igor.khripunov@clearpoint.co.nz>
2017-10-13 10:15:57 +1300

    Reformatted config, changed username

---
+++ run-remote.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ -z $(kubectl get namespaces | grep jenkins) ]]
+then
+	kubectl create ns jenkins
+fi
+kubectl config set-context $(kubectl config current-context) --namespace=jenkins
+if [[ -z $(kubectl get secrets | grep ssh-key-secret) ]]
+then
+	kubectl create secret generic ssh-key-secret --from-file=ssh-privatekey=./id_rsa --from-file=ssh-publickey=./id_rsa.pub
+fi
+if [[ $(kubectl get configmaps --ignore-not-found) ]]
+then
+	kubectl delete configmaps kube-master
+fi
+kubectl create configmap kube-master --from-literal=master.url=$(kubectl cluster-info | grep master | grep -o 'https:\/\/[[:alnum:][:punct:]]*')
+kubectl apply -f jenkins.yml
+kubectl apply -f service.yml
